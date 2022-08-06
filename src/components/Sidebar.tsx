b7593ed1382c223df43f10bf166db59e5807816b
kaikySantos <info.kaiky@gmail.com>
2022-08-06 13:08:15 -0300

    feat: avatar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Avatar } from './Avatar';
+
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
@@ -10,7 +12,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
     
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/kaikySantos.png" />
+        <Avatar src="https://github.com/kaikySantos.png" />
 
         <strong>Kaiky Santos</strong>
         <span>Web Developer</span>
