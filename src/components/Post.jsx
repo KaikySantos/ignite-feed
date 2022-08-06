900b4fb845535198881e3d85342ff7e17d16e658
kaikySantos <info.kaiky@gmail.com>
2022-08-06 10:39:14 -0300

    feat: post

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/diego3g.png" />
+          <div className={styles.authorInfo}>
+            <strong>Diego Fernandes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="6 de Agosto às 09:19" dateTime='2022-08-06 09:19:00'>Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉&nbsp;<a href="#">jane.design/doctorcare</a></p>
+        <p>
+          <a href="#">#novoprojeto</a>&nbsp;
+          <a href="#">#nlw</a>&nbsp;
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
